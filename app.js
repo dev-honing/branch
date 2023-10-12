dfcd7960e1467b1cc2300a0ff8510931d8760421
Tech1997 <dev.honing@gmail.com>
2023-10-12 10:57:33 +0900

    main 브랜치에 내용 커밋 - 향마루 응답처리

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const http = require('http');
-const server = http.createServer((req, res) =>{});
+const http = require("http");
+const server = http.createServer((req, res) =>{
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.write("향마루");
+  res.end();
+});
 server.listen(3500, (err) => {
   if(!err){
-    console.error('서버 작동 X');
+    console.error("서버 작동 X");
   } else {
-    console.log("3500포트에서 작동 O, http://localhost:3500");
+    console.log("3500포트에서 서버 작동 O, http://localhost:3500");
   }
 })
