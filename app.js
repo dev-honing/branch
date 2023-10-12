04e9eab3fe540524b2e055c1af233369a9df03fd
Tech1997 <dev.honing@gmail.com>
2023-10-12 10:44:15 +0900

    조건문 else식의 콘솔내용 수정

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ server.listen(3500, (err) => {
   if(!err){
     console.error('서버 작동 X');
   } else {
-    console.log("3500포트에서 돌아감 http://localhost:3500");
+    console.log("3500포트에서 작동 O, http://localhost:3500");
   }
 })
