62d0bba142dd57c8de599ae71216bbcfbadc7aec
Tech1997 <dev.honing@gmail.com>
2023-10-12 10:43:23 +0900

    server.listen() 메서드의 로직을 조건문으로 작성

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const http = require('http');
 const server = http.createServer((req, res) =>{});
+server.listen(3500, (err) => {
+  if(!err){
+    console.error('서버 작동 X');
+  } else {
+    console.log("3500포트에서 돌아감 http://localhost:3500");
+  }
+})
